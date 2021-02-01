390e02a10ccc9a86082614977ac3c97d08fa61b1
Vasyl Vaskivskyi <vaskivskyi.v@gmail.com>
2021-02-01 18:29:24 +0000

    removed DoG

--- feature_detection.py
+++ feature_detection.py
@@ -1,53 +1,7 @@
-import copy
-
 import numpy as np
 import dask
 import cv2 as cv
-import copyreg
 Image = np.ndarray
-
-
-def img_to_float(img):
-    return cv.normalize(img, None, 0, 1, cv.NORM_MINMAX, cv.CV_32F)
-
-
-def float_to_img(fimg, original_numpy_dtype):
-    cv_np_map = {np.dtype('uint8'): cv.CV_8U,
-                 np.dtype('uint16'): cv.CV_16U,
-                 np.dtype('int8'): cv.CV_8S,
-                 np.dtype('int16'): cv.CV_16S
-                 }
-    cv_dtype = cv_np_map[original_numpy_dtype]
-    alpha = np.iinfo(original_numpy_dtype).min
-    beta = np.iinfo(original_numpy_dtype).max
-    return cv.normalize(fimg, None, alpha, beta, cv.NORM_MINMAX, cv_dtype)
-
-
-def diff_of_gaus(img: Image, low_sigma: int = 5, high_sigma: int = 9):
-    #TODO replace with difference of kernels
-    original_dtype = copy.copy(img.dtype)
-    if original_dtype != np.float32:
-        fimg = img_to_float(img)
-    else:
-        fimg = img
-    low_sigma = cv.GaussianBlur(fimg, (0, 0), sigmaX=low_sigma, dst=None, sigmaY=low_sigma)
-    high_sigma = cv.GaussianBlur(fimg, (0, 0), sigmaX=high_sigma, dst=None, sigmaY=high_sigma)
-    diff = low_sigma - high_sigma
-    del low_sigma, high_sigma
-    if original_dtype == np.float32:
-        # do not need to convert back already in float32
-        return diff
-    else:
-        return float_to_img(diff, original_dtype)
-
-
-def preprocess_image(img: Image):
-    # TODO try to use opencv retina module
-    processed_img = diff_of_gaus(img, 3, 5)
-    if processed_img.dtype != np.uint8:
-        processed_img = cv.normalize(processed_img, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
-
-    return processed_img
 
 
 def _pickle_keypoints(point):
@@ -56,11 +10,13 @@ def _pickle_keypoints(point):
 
 
 def find_features(img):
+    processed_img = img
+    if processed_img.dtype != np.uint8:
+        processed_img = cv.normalize(processed_img, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
 
-    processed_img = preprocess_image(img)
     if processed_img.max() == 0:
         return [], []
-    #detector = cv.MSER_create()
+
     detector = cv.FastFeatureDetector_create(1, True)
     descriptor = cv.xfeatures2d.DAISY_create()
     kp = detector.detect(processed_img)
@@ -78,7 +34,7 @@ def find_features(img):
     if des is None or len(des) < 3:
         return [], []
 
-    #fix problem with pickle
+    # fix problem with pickle
     temp_kp_storage = []
     for point in kp:
         temp_kp_storage.append((point.pt, point.size, point.angle, point.response, point.octave, point.class_id))
